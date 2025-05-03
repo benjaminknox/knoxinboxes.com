1fe6ccb70504a636b39c54f170066bf2cbefe4fd
Benjamin Knox <bknox.contact@gmail.com>
2025-05-03 17:23:39 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import 'reset-css/reset.css';
+	import '@awenovations/aura/dist/tokens/_variables.css';
+  import KnoxInBoxes from "$lib/assets/KnoxInBoxes.png"
+
+</script>
+
+<svelte:head>
+  <title>Knox In Boxes</title>
+</svelte:head>
+
+<img src={KnoxInBoxes} />
+
+<style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+  :global(html, body) {
+		background: var(--aura-background);
+		font: var(--aura-default-regular);
+		color: var(--aura-font-color);
+		height: 100%;
+		width: 100%;
+    text-align: center;
+	}
+
+  img {
+    width: 60vw;
+    margin-top: 8vh;
+  }
+
+</style>
---
+++ www/_app/immutable/chunks/CAI6Mf6H.js
@@ -0,0 +1,1 @@
+import{e}from"./BW6lbmXl.js";e();
