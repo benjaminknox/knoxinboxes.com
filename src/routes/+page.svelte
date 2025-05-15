41e91d0ce052b9fbb8fbf87a211afeb1f025fff5
Benjamin Knox <bknox.contact@gmail.com>
2025-05-14 22:40:05 -0400

    [Coffee]: Buy me one

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
   import 'reset-css/reset.css';
 	import '@awenovations/aura/dist/tokens/_variables.css';
-  import KnoxInBoxes from "$lib/assets/KnoxInBoxes.png"
+  import KnoxInBoxes from "$lib/assets/KnoxInBoxes.png";
 
 </script>
 
+
 <svelte:head>
   <title>Knox In Boxes</title>
+
+  <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="benjaminknox" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
 </svelte:head>
 
-<img src={KnoxInBoxes} />
+<img src={KnoxInBoxes} alt="" />
 
 <style lang="scss">
 	@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
---
+++ www/_app/immutable/entry/start.BVXK6_8w.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/Bi2foeVc.js";export{o as load_css,r as start};
